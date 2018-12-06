8cde0aa8dd810da5d54452988dc74aa5bb1cc8c0
Dax <trogu.davide@gmail.com>
2018-12-06 22:31:15 +0100

    Improved Hashing functions

--- redasm/support/hash.h
+++ redasm/support/hash.h
@@ -1,13 +1,34 @@
 #ifndef HASH_H
 #define HASH_H
 
-#include "../redasm.h"
+#include "../redasm_types.h"
+#include "../redasm_buffer.h"
 
 namespace REDasm {
 namespace Hash {
 
-u16 crc16(const u8* bytes, u32 length);
-u16 crc16(const std::vector<u8>& bytes);
+template<typename T> u16 crc16(const T* data, u64 length) { return crc16(reinterpret_cast<const u8*>(data), length); }
+
+template<> u16 crc16<u8>(const u8* data, u64 length)
+{
+    u8 x;
+    u16 crc = 0xFFFF;
+
+    while(length--)
+    {
+        x = crc >> 8 ^ *data++;
+        x ^= x >> 4;
+        crc = (crc << 8) ^ static_cast<u16>(x << 12) ^ static_cast<u16>(x << 5) ^ static_cast<u16>(x);
+    }
+
+    return crc;
+}
+
+inline u16 crc16(const BufferRef& br) { return crc16(br.data(), br.size()); }
+inline u16 crc16(const BufferRef& br, u64 length) { return crc16(br.data(), std::min(br.size(), length)); }
+inline u16 crc16(const std::string& s) { return crc16(s.data(), s.size()); }
+template<typename T> inline u16 crc16(const std::string& s) { return crc16(s.data(), s.size()); }
+template<typename T, typename A> u16 crc16(const std::vector<T, A>& v) { return crc16(v.data(), v.size()); }
 
 } // namespace Hash
 } // namespace REDasm

ae1f8468d967c589d5e71a3bedf6128b1d255b15
Dax <trogu.davide@gmail.com>
2018-12-18 02:47:58 -0800

    MSVC compatibility

--- redasm/support/utils.cpp
+++ redasm/support/utils.cpp
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <algorithm>
 #include <sstream>
+#include <cctype>
 
 namespace REDasm {
 
@@ -53,7 +54,7 @@ Buffer bytes(const std::string &s)
     Buffer buffer;
     buffer.resize(s.size() / 2);
 
-    for(size_t i = 0, j = 0; i < s.size(); i += 2, j++)
+    for(int i = 0, j = 0; i < s.size(); i += 2, j++)
          buffer[j] = REDasm::byte(s, i);
 
     return buffer;

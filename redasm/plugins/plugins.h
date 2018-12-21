547a144330ee1ebcf55a8028a69f940b69d85f63
Dax <trogu.davide@gmail.com>
2018-12-21 21:14:25 +0100

    REDasm:init(): /tmp path is not mandatory

--- redasm/plugins/plugins.h
+++ redasm/plugins/plugins.h
@@ -30,7 +30,7 @@ FormatPlugin* getFormat(Buffer &buffer);
 AssemblerPlugin* getAssembler(const char* id);
 void setLoggerCallback(Runtime::LogCallback logcb);
 void setStatusCallback(Runtime::LogCallback logcb);
-void init(const std::string &temppath, const std::string &searchpath = std::string());
+void init(const std::string &temppath = std::string(), const std::string &searchpath = std::string());
 
 }
 

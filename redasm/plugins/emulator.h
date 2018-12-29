ed7245b7ff99b56f606f8c702a147d99fc19336e
Dax <trogu.davide@gmail.com>
2018-12-29 16:07:01 +0100

    Emulator: Auto-Target support

--- redasm/plugins/emulator.h
+++ redasm/plugins/emulator.h
@@ -23,6 +23,7 @@ class Emulator
         virtual bool displacement(const Operand& op, u64* value) = 0;
 
     protected:
+        virtual bool setTarget(const InstructionPtr& instruction);
         Buffer& getSegmentMemory(address_t address, offset_t* offset);
         BufferRef getMemory(address_t address);
         BufferRef getStack(offset_t sp);

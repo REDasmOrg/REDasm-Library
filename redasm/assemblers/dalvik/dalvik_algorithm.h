9e31c42e6a67923b24238546e8fb09d1aa079c41
Dax <trogu.davide@gmail.com>
2018-12-30 22:38:27 +0100

    DalvikAlgorithm: Trace and mark imports

--- redasm/assemblers/dalvik/dalvik_algorithm.h
+++ redasm/assemblers/dalvik/dalvik_algorithm.h
@@ -1,11 +1,13 @@
 #ifndef DALVIK_ALGORITHM_H
 #define DALVIK_ALGORITHM_H
 
-#include "../../plugins/assembler/algorithm/controlflow.h"
+#include "../../plugins/assembler/algorithm/algorithm.h"
 
 namespace REDasm {
 
-class DalvikAlgorithm: public ControlFlowAlgorithm
+class DEXFormat;
+
+class DalvikAlgorithm: public AssemblerAlgorithm
 {
     DEFINE_STATES(StringIndexState = UserState, MethodIndexState)
 
@@ -13,9 +15,20 @@ class DalvikAlgorithm: public ControlFlowAlgorithm
         DalvikAlgorithm(DisassemblerAPI* disassembler, AssemblerPlugin* assemblerplugin);
 
     protected:
+        virtual void onDecodeFailed(const InstructionPtr& instruction);
         virtual void onDecodedOperand(const Operand& op, const InstructionPtr& instruction);
+        virtual void onDecoded(const InstructionPtr& instruction);
+        virtual void decodeState(State *state);
         virtual void stringIndexState(const State* state);
         virtual void methodIndexState(const State* state);
+
+    private:
+        void checkImport(const State *state);
+
+    private:
+        DEXFormat* m_dexformat;
+        std::unordered_set<std::string> m_imports;
+        std::unordered_set<address_t> m_methodbounds;
 };
 
 } // namespace REDasm

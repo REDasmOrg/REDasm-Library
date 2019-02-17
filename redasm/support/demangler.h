6b6c76895a966493e3f2b5dbecb57f12cc20917b
Dax <trogu.davide@gmail.com>
2019-02-17 19:11:26 +0100

    Demangler: Added typeName() helper

--- redasm/support/demangler.h
+++ redasm/support/demangler.h
@@ -2,6 +2,7 @@
 #define DEMANGLER_H
 
 #include <string>
+#include <typeinfo>
 
 namespace REDasm {
 namespace Demangler {
@@ -9,6 +10,7 @@ namespace Demangler {
 bool isMSVC(const std::string &s, std::string *result = nullptr);
 bool isMangled(const std::string& s);
 std::string demangled(const std::string& s, bool simplified = true);
+template<typename T> std::string typeName() { return demangled(typeid(T).name()); }
 
 } // namespace Demangler
 } // namespace REDasm

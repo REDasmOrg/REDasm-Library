c57b0e52dc97d54a43f0fe92716400ef85e53028
Dax <trogu.davide@gmail.com>
2019-03-05 12:31:10 +0100

    Itanium demangler integration

--- redasm/support/demangler.cpp
+++ redasm/support/demangler.cpp
@@ -1,5 +1,6 @@
 #include "demangler.h"
-#include <undname.h> // MSVC Demangler
+#include <undname.h>  // MSVC Demangler
+#include <demangle.h> // Itanium Demangler
 #include <vector>
 #include <regex>
 
@@ -28,6 +29,17 @@ std::string demangleMSVC(const std::string& s, bool simplified) {
     return v.data();
 }
 
+std::string demangleItanium(const std::string& s) {
+    char* pres = cplus_demangle_v3(s.c_str(), DMGL_NO_OPTS);
+
+    if(!pres)
+        return s;
+
+    std::string res = pres;
+    free(pres);
+    return res;
+}
+
 bool isMSVC(const std::string &s, std::string* result) {
     std::smatch match;
 
@@ -44,7 +56,15 @@ bool isMangled(const std::string &s) {
     if(s.empty())
         return false;
 
-    return Demangler::isMSVC(s);
+    return Demangler::isMSVC(s) || Demangler::isItanium(s);
+}
+
+bool isItanium(const std::string &s, std::string *result)
+{
+    if(s.empty() || s.front() != '_')
+        return false;
+
+    return (s.find("_Z") == 0) || (s.find("__Z") == 0);
 }
 
 std::string demangled(const std::string &s, bool simplified) {
@@ -52,6 +72,8 @@ std::string demangled(const std::string &s, bool simplified) {
 
     if(Demangler::isMSVC(s, &result))
         return demangleMSVC(result, simplified);
+    if(Demangler::isItanium(s))
+        return demangleItanium(s);
 
     return s;
 }
--- redasm/support/demangler.h
+++ redasm/support/demangler.h
@@ -8,6 +8,7 @@ namespace REDasm {
 namespace Demangler {
 
 bool isMSVC(const std::string &s, std::string *result = nullptr);
+bool isItanium(const std::string &s, std::string *result = nullptr);
 bool isMangled(const std::string& s);
 std::string demangled(const std::string& s, bool simplified = true);
 template<typename T> std::string typeName() { return demangled(typeid(T).name()); }

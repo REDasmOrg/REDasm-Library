82a5d205028f59710f63baa3540070ab833ab63b
Dax <trogu.davide@gmail.com>
2019-06-10 16:28:10 +0200

    Added FindREDasm.cmake

--- redasm/FindREDasm.cmake
+++ redasm/FindREDasm.cmake
@@ -1,3 +1,26 @@
+set(REDASM_FOUND TRUE)
+set(REDASM_LIBRARY_NAME "LibREDasm${CMAKE_SHARED_LIBRARY_SUFFIX}")
+
+foreach(MODULE ${CMAKE_MODULE_PATH})
+    if(NOT DEFINED REDASM_INCLUDE_PATH AND EXISTS "${MODULE}/redasm.h")
+        set(REDASM_INCLUDE_PATH "${MODULE}/..")
+    endif()
+
+    if(NOT DEFINED REDASM_LIBRARY_PATH AND EXISTS "${MODULE}/../${REDASM_LIBRARY_NAME}")
+        set(REDASM_LIBRARY_PATH "${MODULE}/../${REDASM_LIBRARY_NAME}")
+    endif()
+endforeach()
+
+if(WIN32)
+    if(NOT DEFINED REDASM_INCLUDE_PATH)
+        set(REDASM_INCLUDE_PATH ${CMAKE_CURRENT_LIST_DIR})
+    endif()
+
+    if(NOT DEFINED REDASM_LIBRARY_PATH)
+        set(REDASM_LIBRARY_PATH "${CMAKE_SOURCE_DIR}/../${REDASM_LIBRARY_NAME}")
+    endif()
+endif()
+
 function(redasm_plugin P_NAME)
     set(CMAKE_CXX_STANDARD 11)
     set(CMAKE_SKIP_BUILD_RPATH TRUE)
@@ -13,7 +36,7 @@ function(redasm_plugin P_NAME)
     endif()
 
     if(DEFINED REDASM_LIBRARY_PATH)
-        target_link_libraries(${P_NAME} PRIVATE "${REDASM_LIBRARY_PATH}/LibREDasm${CMAKE_SHARED_LIBRARY_SUFFIX}")
+        target_link_libraries(${P_NAME} PRIVATE ${REDASM_LIBRARY_PATH})
     else()
         target_link_libraries(${P_NAME} PRIVATE LibREDasm)
     endif()

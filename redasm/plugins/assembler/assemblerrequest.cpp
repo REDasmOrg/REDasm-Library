32c39111fe8a6a4a823040700f5108a6dd279ee0
Dax <trogu.davide@gmail.com>
2019-07-30 16:45:13 +0200

    AssemblerRequest: NULL checks

--- redasm/plugins/assembler/assemblerrequest.cpp
+++ redasm/plugins/assembler/assemblerrequest.cpp
@@ -4,7 +4,21 @@
 namespace REDasm {
 
 AssemblerRequest::AssemblerRequest() { }
-AssemblerRequest::AssemblerRequest(const char *id, const char *mode): id(id) { this->mode = mode ? mode : id; }
+
+AssemblerRequest::AssemblerRequest(const char *id, const char *mode)
+{
+    if(mode)
+        this->mode = mode;
+
+    if(!id)
+        return;
+
+    if(this->mode.empty())
+        this->mode = id;
+
+    this->id = id;
+}
+
 bool AssemblerRequest::modeIs(const String &mode) const { return this->mode == mode; }
 bool AssemblerRequest::idIs(const String& id) const { return this->id == id; }
 REDasm::AssemblerRequest::operator String() const { return id; }

5e7a5cf25db7635862c994f11f1c48f457416c5f
Dax <trogu.davide@gmail.com>
2019-08-07 16:15:38 +0200

    CachedInstruction: Improved validation

--- redasm/disassembler/listing/cachedinstruction.cpp
+++ redasm/disassembler/listing/cachedinstruction.cpp
@@ -6,7 +6,7 @@ namespace REDasm {
 CachedInstruction::CachedInstruction(): m_pimpl_p(new CachedInstructionImpl(this)) { }
 CachedInstruction::CachedInstruction(InstructionCache *cache, Instruction *instruction): m_pimpl_p(new CachedInstructionImpl(this, cache, instruction)) { }
 Instruction* CachedInstruction::operator->() const { return this->get(); }
-REDasm::CachedInstruction::operator bool() const { PIMPL_P(const CachedInstruction); return p; }
+REDasm::CachedInstruction::operator bool() const { PIMPL_P(const CachedInstruction); return p && (p->m_instruction != nullptr); }
 size_t CachedInstruction::referenceCount() const { PIMPL_P(const CachedInstruction); return p->referenceCount(); }
 Instruction *CachedInstruction::get() const { PIMPL_P(const CachedInstruction); return p->get(); }
 

7b39caf9a81518de129e0e6bdc7785d02fbd733c
Dax <contact@antoniodavide.dev>
2020-03-07 11:08:25 +0100

    ListAdapter: Added support for array references or by copying it

--- impl/types/container_impl.h
+++ impl/types/container_impl.h
@@ -6,13 +6,27 @@
 
 namespace REDasm {
 
-template<typename T> class ListAdapterT: public ListAdapter<T>
+template<typename T> class ListAdapterT: public ListAdapter<T>    // Copy
 {
     public:
         typedef std::deque<T> Container;
 
     public:
-        ListAdapterT(Container* c): m_c(c) { }
+        ListAdapterT(const Container& c): m_c(c) { }
+        size_t size() const override { return m_c->size(); }
+        const T& at(size_t idx) const override { return m_c->operator[](idx); }
+
+    private:
+        Container m_c;
+};
+
+template<typename T> class ListAdapterRefT: public ListAdapter<T> // Pointer
+{
+    public:
+        typedef std::deque<T> Container;
+
+    public:
+        ListAdapterRefT(Container* c): m_c(c) { }
         size_t size() const override { return m_c->size(); }
         const T& at(size_t idx) const override { return m_c->operator[](idx); }
 
@@ -20,6 +34,7 @@ template<typename T> class ListAdapterT: public ListAdapter<T>
         Container* m_c;
 };
 
-template<typename T> ListAdapter<T>* list_adapter(typename ListAdapterT<T>::Container* t) { return new ListAdapterT<T>(t); }
+template<typename T> ListAdapter<T>* list_adapter(const typename ListAdapterT<T>::Container& t) { return new ListAdapterT<T>(t); }
+template<typename T> ListAdapter<T>* list_adapter(typename ListAdapterRefT<T>::Container* t) { return new ListAdapterRefT<T>(t); }
 
 } // namespace REDasm

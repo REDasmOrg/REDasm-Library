b8d4d335066d763598f56aea98eb33ac460378a4
Dax <contact@antoniodavide.dev>
2020-12-16 22:23:28 +0100

    Removed unused files

--- rdcore/types/factory.cpp
+++
@@ -1,8 +0,0 @@
-#include "factory.h"
-#include "../support/error.h"
-
-StructureType* TypeFactory::createStructure() { return new StructureType(); }
-
-Type* TypeFactory::createType(rd_type type)
-{
-}

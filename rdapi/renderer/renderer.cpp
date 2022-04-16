1556c14d80c75ce5445571ade00a60be5a42eb76
Dax <contact@antoniodavide.dev>
2022-04-16 19:16:45 +0200

    Added null checks

--- rdapi/renderer/renderer.cpp
+++ rdapi/renderer/renderer.cpp
@@ -5,12 +5,12 @@ void RDRenderer_Indent(RDRenderer* r, size_t n) { CPTR(Renderer, r)->renderInden
 void RDRenderer_Signed(RDRenderer* r, s64 val) {  CPTR(Renderer, r)->renderSigned(val);  }
 void RDRenderer_Unsigned(RDRenderer* r, u64 val) { CPTR(Renderer, r)->renderUnsigned(val); }
 void RDRenderer_Reference(RDRenderer* r, rd_location ref) { CPTR(Renderer, r)->renderReference(ref); }
-void RDRenderer_Mnemonic(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderMnemonic(s, theme); }
-void RDRenderer_MnemonicWord(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderMnemonicWord(s, theme); }
-void RDRenderer_Register(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderRegister(s); }
-void RDRenderer_Constant(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderConstant(s); }
-void RDRenderer_Text(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderText(s); }
-void RDRenderer_Themed(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderText(s, theme); }
+void RDRenderer_Mnemonic(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderMnemonic(s ? s : "mnemonic???", theme); }
+void RDRenderer_MnemonicWord(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderMnemonicWord(s ? s : "mnemonic???", theme); }
+void RDRenderer_Register(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderRegister(s ? s : "reg???"); }
+void RDRenderer_Constant(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderConstant(s ? s : "cnst???"); }
+void RDRenderer_Text(RDRenderer* r, const char* s) { CPTR(Renderer, r)->renderText(s ? s : "text???"); }
+void RDRenderer_Themed(RDRenderer* r, const char* s, rd_type theme) { CPTR(Renderer, r)->renderText(s ? s : "text???", theme); }
 void RDRenderer_Unknown(RDRenderer* r) { CPTR(Renderer, r)->renderUnknown(); }
 
 const char* RD_GetInstruction(RDContext* ctx, rd_address address)
